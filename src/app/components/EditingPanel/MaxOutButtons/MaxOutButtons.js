ece7064b0c07ea5bd7e54b3b7492aac8ee481e7d
LucianoInfanti <lucianoinfanti369@gmail.com>
2023-06-22 22:42:14 -0300

    adding shake animation + max out btns styling and function

--- src/app/components/EditingPanel/MaxOutButtons/MaxOutButtons.js
+++ src/app/components/EditingPanel/MaxOutButtons/MaxOutButtons.js
@@ -1,5 +1,7 @@
 import styles from "./MaxOutButtons.module.css";
 import { globalStore } from "@/stores/globalStore";
+import { motion } from "framer-motion";
+import React, { useState } from "react";
 
 const MaxOutButtons = () => {
   const {
@@ -12,9 +14,15 @@ const MaxOutButtons = () => {
     setEngravingLevel,
   } = globalStore();
 
-  const toggleModal = () => setModalIsOpen(!modalIsOpen);
+  const [shakeHero, setShakeHero] = useState(false);
+  const [shakeComposition, setShakeComposition] = useState(false);
+
+  const isAnySlotFilled = Object.values(slots).some((slot) => slot);
 
   const handleMaxOutHero = () => {
+    const { selectedSlot, slots } = globalStore.getState();
+    if (!slots[selectedSlot]) return;
+
     setStars(5, selectedSlot);
     setSignatureLevel(30, selectedSlot);
     setAwakeningLevel("Ascended", selectedSlot);
@@ -31,12 +39,41 @@ const MaxOutButtons = () => {
       setEngravingLevel(80, slot);
     });
   };
+
+  const handleMaxOutHeroClick = () => {
+    if (!slots[selectedSlot]) {
+      setShakeHero(true);
+      setTimeout(() => setShakeHero(false), 200);
+    } else {
+      handleMaxOutHero();
+    }
+  };
+
+  const handleMaxOutCompClick = () => {
+    if (!isAnySlotFilled) {
+      setShakeComposition(true);
+      setTimeout(() => setShakeComposition(false), 200);
+    } else {
+      handleMaxOutComposition();
+    }
+  };
+
   return (
     <div className={styles.maxOutSection}>
-      <button className={styles.maxOutButton} onClick={handleMaxOutHero}>
+      <button
+        className={`${styles.maxOutHeroButton} ${
+          slots[selectedSlot] ? styles.selectable : styles.disabled
+        } ${shakeHero ? styles.shake : ""}`}
+        onClick={handleMaxOutHeroClick}
+      >
         Max out hero
       </button>
-      <button className={styles.maxOutButton} onClick={handleMaxOutComposition}>
+      <button
+        className={`${styles.maxOutCompButton} ${
+          isAnySlotFilled ? styles.selectable : styles.disabled
+        } ${shakeComposition ? styles.shake : ""}`}
+        onClick={handleMaxOutCompClick}
+      >
         Max out composition
       </button>
     </div>
